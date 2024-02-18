7d5a34c92d533054c90305a49d307bab92e299a7
Ceptea <c-e-p-t-e@outlook.com>
2024-02-18 02:57:51 -0500

    Fix untested js.

--- help/script.js
+++ help/script.js
@@ -1,14 +1,13 @@
 let minecraft_location = ""
+console.log(navigator.userAgent)
 if (navigator.userAgent.includes("Linux")) {
     minecraft_location = "~/.minecraft/"
 
 } else if (navigator.userAgent.includes("Windows")) {
     minecraft_location = "%appdata%\\.minecraft\\"
 } else {
-    return
+    minecraft_location = ".minecraft"
 }
-
-
 
 document.getElementById("crash-log").innerHTML = `${minecraft_location}crash-reports`
 document.getElementById("log").innerHTML = `${minecraft_location}logs`
